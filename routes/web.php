019c79dd5a977fd34ff4c5be1cc52647f9de57be
NGONYOKU <ngonyoku001@gmail.com>
2023-05-26 02:18:37 +0300

    Controllers Added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -14,30 +15,11 @@ use App\Models\Listing;
 |
 */
 
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+//All Listings
+Route::get('/', [ListingController::class, 'index']);
 
 //Single Listing
-Route::get('/listings/{listing}', function(Listing $listing){
-
-    //Route Model Binding
-    return view('listing', [
-        'listing' => $listing
-    ]);
-    // $listing = Listing::find($id);
-
-    // if ($listing){
-    //     return view('listing', [
-    //         'listing' => $listing
-    //     ]);
-    // } else {
-    //     abort('404');
-    // }
-});
+Route::get('/listings/{listing}',[ListingController::class, 'show']);
 
 
 // Route::get('/hello', function() {
