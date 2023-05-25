940c15053cbbb5ff42cc6b4b8aa59bac9217b990
NGONYOKU <ngonyoku001@gmail.com>
2023-05-25 17:35:47 +0300

    Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,21 @@ Route::get('/', function () {
 });
 
 //Single Listing
-Route::get('/listings/{id}', function($id){
+Route::get('/listings/{listing}', function(Listing $listing){
+
+    //Route Model Binding
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
+    // $listing = Listing::find($id);
+
+    // if ($listing){
+    //     return view('listing', [
+    //         'listing' => $listing
+    //     ]);
+    // } else {
+    //     abort('404');
+    // }
 });
 
 
